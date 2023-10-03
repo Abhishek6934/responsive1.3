43f6bd5c04b7589f38833c4b1cb7a64b5d9022ad
Abhishek6934 <140361773+Abhishek6934@users.noreply.github.com>
2023-10-03 20:24:38 +1100

    Create WelcomeItem.vue

---
+++ WelcomeItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="WelcomeItem.vue">
+    <h3>{{ projectTitle }}</h3>
+    <p>{{ projectDescription }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['projectTitle', 'projectDescription']
+}
+</script>
+
+<style scoped>
+.project-item {
+  background-color: #fff;
+  padding: 20px;
+  margin: 10px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+</style>
